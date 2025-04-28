0ab3877c5f800fa0b6c899a3b731def4ec03a173
camillafalk <ccamilla.falk@gmail.com>
2025-04-28 16:12:07 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Camilla Falk on 2025-04-28.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Camilla Falk on 2025-04-28.
+//
+
+import Testing
+@testable import HabitTracker
+
+struct HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
