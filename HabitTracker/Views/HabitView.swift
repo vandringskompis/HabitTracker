b6a49a6b2f3a1d78ef139da08af985f7223dffa2
camillafalk <ccamilla.falk@gmail.com>
2025-04-30 17:23:29 +0200

    Added basic FSCalendar to HabitView.

--- HabitTracker/Views/HabitView.swift
+++ HabitTracker/Views/HabitView.swift
@@ -27,24 +27,33 @@ struct HabitView: View {
                         .fontDesign(.monospaced)
                         .shadow(radius: 10.0, x: 20, y: 10)
             
+                    /* if let title = habit.title {
+                         Text(title)
+                     }*/
                     Spacer()
                     
-                   /* if let title = habit.title {
-                        Text(title)
-                    }*/
+                   
                 }
+                
+                HStack{
+                    CalendarView()
+                        .padding(30)
+                }
+                
                 Spacer()
                 
                 HStack{
                     Text("Streak: \n     0")
                         .font(.title)
+                    //Text("\(habit.streak)")
                     
                     Spacer()
                     
                     Text("Longest Streak: \n              6")
                         .font(.title)
+                    //Text("\(habit.longestStreak)")
                     
-                    //Text("\(habit.streak)")
+                    
                 }
                 .padding()
             }
